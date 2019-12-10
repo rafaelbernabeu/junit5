b7d31377f6f69b3194d2b4dddbcbfba363acbf5f
Rafael Bernabeu <rbbernabeu@gmail.com>
2019-12-09 22:51:56 -0300

    enable multithread execution

--- src/test/java/br/bernabeu/AnnotatedTests.java
+++ src/test/java/br/bernabeu/AnnotatedTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,6 +18,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class AnnotatedTests {
 
     @ParameterizedTest
